f3786b48dc86872034c0b5f9d929287651d642f1
maarten peels <maartenpeels@gmail.com>
2018-09-25 10:55:03 +0200

    Seed initial data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $admin = new \App\UserType();
+        $admin->name = "admin";
+        $admin->save();
+
+        $employee = new \App\UserType();
+        $employee->name = "employee";
+        $employee->save();
+
+        $hours = new \App\WorkingHours();
+        $hours->start_time = '09:00';
+        $hours->stop_time = '17:00';
+        $hours->save();
+
+        $user = new \App\User();
+        $user->first_name = 'Maarten';
+        $user->middle_name = '';
+        $user->last_name = 'Peels';
+        $user->email = 'maartenpeels@gmail.com';
+        $user->password = bcrypt('password');
+        $user->working_hours_id = $hours->id;
+        $user->user_type_id = $admin->id;
+        $user->save();
     }
 }
